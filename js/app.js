1d4dbfc8e154b4163ba6f0c9643bf3c7b589e29b
Christopher Thomson <Christopher.thomson42@gmail.com>
2016-05-13 12:26:33 -0700

    Submission ready file
    Having completed my new win state I feel ready for review.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
-// Enemies our player must avoid
+// Enemy bugs our player must avoid
 var Enemy = function(x, y, speed, width, height) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
+    // Variables applied to each of our instances go here
     // a helper we've provided to easily load images
     this.x = x;
     this.y = y;
-    this.width = 90;
-    this.height = 90;
-    this.speed = Math.floor(100 + (Math.random()*400) + 1);
+    this.width = 50;
+    this.height = 50;
+    this.speed = Math.floor(100 + (Math.random() * 400) + 1); //this alows for the bugs to radomly select a new speed on start.
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -17,18 +14,12 @@ var Enemy = function(x, y, speed, width, height) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    if(this.x < 505){
+    if (this.x < 505) { // this alows for bugs to randomly slect a new speed on x value reset
         this.x = this.x + this.speed * dt;
-    }
-
-    else {
+    } else {
         this.x = 0;
-        this.speed = Math.floor(100 + (Math.random()*400) + 1);
-
+        this.speed = Math.floor(100 + (Math.random() * 400) + 1);
     }
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.  
 };
 
 // Draw the enemy on the screen, required method for game
@@ -36,6 +27,7 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+//The 4 bug objects are created here
 var enemy0 = new Enemy(-100, 310);
 
 var enemy1 = new Enemy(-100, 50);
@@ -44,96 +36,123 @@ var enemy2 = new Enemy(-100, 225);
 
 var enemy3 = new Enemy(-100, 140);
 
-// Now write your own player class    
+//The constructor for the star goal for the player is made here
+var Star = function() {
+    this.x = 200;
+    this.y = -5;
+    this.width = 50;
+    this.height = 50;
+    this.sprite = 'images/Star.png';
 
+};
+//enpty update function for star nessary for engien
+Star.prototype.update = function() {
+
+};
+Star.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+//evocing new star object
+var star = new Star();
+
+// Player class constructor  
 var Player = function() {
     this.x = 200;
     this.y = 400;
-    this.width = 90;
-    this.height = 90;
+    this.width = 50;
+    this.height = 50;
     this.sprite = 'images/char-boy.png';
 };
-Player.prototype.update = function(){
+//Used to generate realtime updates on hitting bugs or the goal
+Player.prototype.update = function() {
+    this.checkCollisons();
+    this.checkWin();
 };
-Player.prototype.handleInput =function(inputKeys){
-    switch(inputKeys) {
-        case'left' :
-        if(this.x - 101 < 0) {
-            this.x = 0;
-        }
-        
-        else{
-            this.x -= 101;
-        }
-        break;
-        
-        case'right' :
-        if(this.x + 101 >=404) {
-            this.x = 404;
-        }
-        else{
-            this.x +=101;
-        }
-        break;
-        
-        case 'up' :
-        if(this.y - 85 < 0) {
-            this.y = -15;
-        }
-        else{
-            this.y -= 85;
-        }
+//Used to conver player input into character action.
+Player.prototype.handleInput = function(inputKeys) {
+    switch (inputKeys) {
+        case 'left':
+            if (this.x - 101 < 0) {
+                this.x = 0;
+            } else {
+                this.x -= 101;
+            }
+            break;
 
-        break;
+        case 'right':
+            if (this.x + 101 >= 404) {
+                this.x = 404;
+            } else {
+                this.x += 101;
+            }
+            break;
+
+        case 'up':
+            if (this.y - 85 < 0) {
+                this.y = -15;
+            } else {
+                this.y -= 85;
+            }
+
+            break;
 
         case 'down':
-        if(this.y + 85 >= 404){
-            this.y = 404;
-        }
+            if (this.y + 85 >= 404) {
+                this.y = 404;
+            } else {
+                this.y += 85;
+            }
+            break;
 
-        else{
-            this.y += 85;
-        }
-        break;
-       
 
     }
 };
+//When evoced by Player.update made to reset the game pice position.
 Player.prototype.checkCollisons = function() {
-    if (this.x < Enemy.x + Enemy.width &&
-        this.x + this.width > Enemy.x &&
-        this.y < Enemy.y + Enemy.height &&
-        this.height + this.y > Enemy.y) {
-        this.reset();
-    console.log('collision detected!');
+    allEnemies.forEach(function(enemy) {
+
+        if (this.x < enemy.x + enemy.width &&
+            this.x + this.width > enemy.x &&
+            this.y < enemy.y + enemy.height &&
+            this.height + this.y > enemy.y) {
+            this.reset();
+            console.log('collision detected!');
         }
+    }, player);
 };
+//Renders the player
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+//Sets starting point
 Player.prototype.reset = function() {
-  this.x = 200;
-  this.y = 400;
+    this.x = 200;
+    this.y = 400;
 };
-var player = new Player(); 
-// This class requires an update(), render() and
-// a handleInput() method.
+//When evoced by Player.update will prompt the you win sign.
+Player.prototype.checkWin = function() {
+    if (this.x < star.x + star.width &&
+        this.x + this.width > star.x &&
+        this.y < star.y + star.height &&
+        this.height + this.y > star.y) {
+        star.sprite = 'images/You Win.png';
+        star.x = 100;
+        star.y = 150;
+        console.log('You Win!');
+    }
+};
+//Makes the new player object.
+var player = new Player();
 
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-
+//All enemy objects are placed in an array called allEnemies for collison checking
 var allEnemies = [];
 
-allEnemies.push(enemy0,enemy1,enemy2,enemy3);
-
-
-// Place the player object in a variable called player
+allEnemies.push(enemy0, enemy1, enemy2, enemy3);
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+// This listens for key presses and sends the keys to the Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
